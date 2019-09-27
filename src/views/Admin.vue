fd1de00352cd1814115e6e68e205c7f5f31c80a9
mrpinkcat <gatien.anizan@gmail.com>
2019-09-27 02:32:41 +0200

    Admin route

---
+++ src/views/Admin.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="admin">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import Logo from './../components/Logo.vue';
+
+@Component({
+  components: {
+    Logo,
+  },
+})
+export default class Admin extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import './../scss/index.scss';
+
+.admin {
+
+}
+</style>
