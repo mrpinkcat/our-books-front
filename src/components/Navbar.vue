e318231be0d4b32106b43e2370fdca099788bf9c
mrpinkcat <gatien.anizan@gmail.com>
2019-09-28 21:59:09 +0200

    dropdown icon size

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -92,8 +92,8 @@ export default class Navbar extends Vue {
       padding: 0;
 
       img {
-        width: 30px;
-        height: 30px;
+        width: 20px;
+        height: 20px;
       }
     }
   }
