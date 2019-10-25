0d45d0a8585246cf2261c1f308f61b8f416cce43
mrpinkcat <gatien.anizan@gmail.com>
2019-10-25 10:59:17 +0200

    Vuex persist

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,10 +1,27 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from 'axios';
+import VuexPersistence from 'vuex-persist';
 
 Vue.use(Vuex);
 
+interface state {
+  logged: boolean;
+
+  fullName: string | undefined;
+  username: string | undefined;
+  birthDate: Date | undefined;
+  rank: string | undefined;
+  token: string | undefined;
+  libraryId: string | undefined;
+}
+
+// @ts-ignore
+const plugins: Array<Plugin<state>> = [new VuexPersistence().plugin];
+
 export default new Vuex.Store({
+  plugins,
+
   state: {
     logged: false,
 
@@ -17,40 +34,41 @@ export default new Vuex.Store({
   },
 
   mutations: {
-    CHANGE_LIBRARY: (state, libraryId) => {
-      axios.post('/user/library', { libraryId }, { headers: {Authorization: `bearer ${state.token}` } })
+    CHANGE_LIBRARY(argState: state, libraryId) {
+      axios.post('/user/library', { libraryId }, { headers: {Authorization: `bearer ${argState.token}` } })
       .then(() => {
-        state.libraryId = libraryId;
+        argState.libraryId = libraryId;
       })
       .catch((err) => {
         console.error(err);
       });
     },
-    LOGIN: (state, userInfo) => {
-      state.logged = true;
-      state.fullName = userInfo.fullName;
-      state.username = userInfo.username;
-      state.birthDate = (new Date(userInfo.birthDate) as any);
-      state.rank = userInfo.rank;
-      state.token = userInfo.token;
-      state.libraryId = userInfo.libraryId;
+    LOGIN(argState: state, userInfo) {
+      argState.logged = true;
+      argState.fullName = userInfo.fullName;
+      argState.username = userInfo.username;
+      argState.birthDate = (new Date(userInfo.birthDate) as any);
+      argState.rank = userInfo.rank;
+      argState.token = userInfo.token;
+      argState.libraryId = userInfo.libraryId;
     },
   },
+
   getters: {
-    token: (state): string | undefined => {
-      return state.token;
+    token(argState: state): string | undefined {
+      return argState.token;
     },
-    fullName: (state): string | undefined => {
-      return state.fullName;
+    fullName(argState: state): string | undefined {
+      return argState.fullName;
     },
-    rank: (state): string | undefined => {
-      return state.rank;
+    rank(argState: state): string | undefined {
+      return argState.rank;
     },
-    logged: (state): boolean => {
-      return state.logged;
+    logged(argState: state): boolean {
+      return argState.logged;
     },
-    libraryId: (state): boolean | undefined => {
-      return state.libraryId;
+    libraryId(argState: state): string | undefined {
+      return argState.libraryId;
     },
   },
 });
