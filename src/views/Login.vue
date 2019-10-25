f0188c6eb2c6cf4e63c6805df0778db7b15ca92d
mrpinkcat <gatien.anizan@gmail.com>
2019-10-25 09:12:10 +0200

    Login design

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="login">
-    <label for="username">Nom d'utilisateur</label>
-    <input type="text" name="username" v-model="username" :class="{ error: errorField === 'username'}">
+    <div class="login-container">
+      <logo/>
+      <label for="username">Nom d'utilisateur</label>
+      <input type="text" name="username" v-model="username" :class="{ error: errorField === 'username'}" @keydown.enter="login()">
 
-    <label for="password">Mot de passe</label>
-    <input type="password" name="password" v-model="password" :class="{ error: errorField === 'password'}">
+      <label for="password">Mot de passe</label>
+      <input type="password" name="password" v-model="password" :class="{ error: errorField === 'password'}" @keydown.enter="login()">
 
-    <button @click="login()" :disabled="username == '' || password == ''">Connexion</button>
+      <button @click="login()" :disabled="username == '' || password == ''">Connexion</button>
 
-    <span class="error-hint" v-if="error !== ''">{{error}}</span>
+      <span class="error-hint" v-if="errorField === 'username'">Nom d'utilisateur incorrect</span>
+      <span class="error-hint" v-if="errorField === 'password'">Mot de passe incorrect</span>
+    </div>
   </div>
 </template>
 
@@ -60,12 +64,55 @@ export default class Login extends Vue {
 .login {
   display: flex;
   justify-content: center;
-  align-items: center;
-  flex-direction: column;
   height: 100%;
+  background: url('./../assets/login-background.jpg') no-repeat center center fixed;
+  background-size: cover;
+  
+  .login-container {
+    background: $background-primary;
+    padding: 50px;
+    position: fixed;
+    top: 20%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    box-shadow: 0 19px 27px -11px rgba(0, 0, 0, 0.3);
+    border-radius: 6px;
 
-  .error {
-    background: red;
+    .logo {
+      margin-bottom: 20px;
+    }
+
+    label {
+      margin-bottom: 10px;
+    }
+
+    input {
+      margin-bottom: 20px;
+      border-radius: 6px;
+      border-width: 1px;
+      border-style: solid;
+      border-color: $secondary;
+      padding: 6px 16px;
+    }
+
+    button {
+      background: $secondary;
+      color: $background-secondary;
+      padding: 6px 16px;
+      border-radius: 6px;
+      box-shadow: 0 10px 27px -11px rgba(0, 0, 0, 0.3);
+    }
+
+    .error-hint {
+      margin-top: 20px;
+      color: red;
+    }
+  
+    .error {
+      border-color: red;
+    }
   }
 }
 </style>
